57a80ddeae666b131c87b36173064fd0b5529391
SkyeYang001 <143848467+SkyeYang001@users.noreply.github.com>
2024-02-14 14:48:04 -0800

    Start
    2/14/24

--- 1-flattening.js
+++ 1-flattening.js
@@ -1,5 +1,6 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
+
 // Your code here.
-
-
+let reduce = (a + b + c) => a.concat() + b.concat() + c.concat();
+console.log(reduce(arrays))
 // → [1, 2, 3, 4, 5, 6]
